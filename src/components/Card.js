23e11adc378b6a3f9e5c5c5b8f17e83ffbc9c112
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-06-18 21:38:58 -0500

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card(){
+    return (
+        <div className="Card">
+            <p>Advice</p>
+            <button>click</button>
+        </div>
+    )
+}
+export default Card;
