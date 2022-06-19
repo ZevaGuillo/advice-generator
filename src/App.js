6f1bb8b1efddf9cd3c50688469a9ff9d73b57ced
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-06-18 23:52:18 -0500

    Added attribution

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <Card/>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a className='github' href="https://github.com/ZevaGuillo">
+        <i class="fab fa-github" aria-hidden="true"></i> ZevaGuillo</a>.
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 import getAdvice from "../services/adviceAPI";
 import icon from '../images/icon-dice.svg'
 import divider from '../images/pattern-divider-desktop.svg'
@@ -6,12 +7,12 @@ import divider from '../images/pattern-divider-desktop.svg'
 function Card(){
     const [advice, setAdvice] = React.useState('');
     const [adviceId, setAdviceId] = React.useState('');
-    const [loading, setLoading] = React.useState(false);
+    const [loading, setLoading] = React.useState(true);
 
     React.useEffect(()=>{
         
-        setLoading(true);
         randomAdvice();
+        setLoading(false)
     },[])
 
     async function randomAdvice(){
@@ -24,12 +25,13 @@ function Card(){
         <div className="card">
             
             {loading?
+                <ClipLoader color='#00A854' loading={loading} size={50} />
+            :
                 <div className="advice">
                     <p className="advice-id">ADVICE #{adviceId}</p>
                     <p>"{advice}"</p>
                 </div>
-            :
-                <span>Aqui va un spiner</span>}
+            }
             
             <img src={divider} alt="Divider"/>
             <div className="button" onClick={randomAdvice}>
